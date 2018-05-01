88c1b5eb30bebf6825bde4ab8ef7903cb1a3294d
Jakub Lichman <lichman.jakub@gmail.com>
2018-05-01 17:55:28 +0200

    BackendUtils and Context classes created and implemented.

---
+++ src/HW4/src/cd/backend/codegen/BackendUtils.java
@@ -0,0 +1,78 @@
+package cd.backend.codegen;
+
+import java.util.List;
+
+import cd.Config;
+import cd.backend.codegen.RegisterManager.Register;
+import cd.ir.Ast.ClassDecl;
+import cd.ir.Symbol.ClassSymbol;
+
+/**
+ * Utilities for saving and restoring of registers to the stack. Computing right offsets and asm label names.
+ * 
+ * @author limo
+ *
+ */
+public class BackendUtils {
+
+	// returns method label
+	public static String getMethodLabel(String c_name, String m_name) {
+		return "func_" + c_name + "_" + m_name;
+	}
+	
+	// returns vtable label
+	public static String getVTableLabel(String c_name) {
+		return "vtable_" + c_name;
+	}
+	
+	// pushes given registers onto the stack
+	public static void saveRegisters(AssemblyEmitter emit, List<Register> regs) {
+		emit.emitComment("Saving registers onto the stack");
+		for(Register r : regs) {
+			emit.emit("pushl", r);
+		}
+	}
+	
+	// restores given registers from the stack
+	public static void restoreRegisters(AssemblyEmitter emit, List<Register> regs) {
+		emit.emitComment("Restoring registers from the stack");
+		// we need to iterate in reverse order because its a stack
+		for(int i = regs.size() - 1; i >= 0; i--) {
+			emit.emit("popl", regs.get(i));
+		}
+	}
+	
+	// pushes and alignes arguments on the stack (in reverse order i.e. they increase to bigger addresses)
+	public static void pushArguments(AssemblyEmitter emit, List<String> args, int offset) {
+		emit.emitComment("Pushing arguments on the stack");
+		int size = allocSpaceSize(offset, args.size());
+		emit.emit("subl", AssemblyEmitter.constant(size), Register.ESP);
+		for(int i = 0; i < args.size(); i++) {
+			emit.emitStore(args.get(i), i * Config.SIZEOF_PTR, Register.ESP);
+		}
+	}
+	
+	// returns offset that should be substracted from stack pointer
+	public static int allocSpaceSize(int offset, int argsCount) {
+		// we need to have 16 bite alignment otherwise we got seg faults  
+		int argSpace = argsCount * Config.SIZEOF_PTR;
+		return (offset - argSpace) % 16 + 16 + argSpace;
+	}
+	
+	// pops arguments from the stack
+	public static void popArguments(AssemblyEmitter emit, List<String> args, int offset) {
+		emit.emitComment("Poping arguments");
+		int size = allocSpaceSize(offset, args.size());
+		emit.emit("addl", AssemblyEmitter.constant(size), Register.ESP);
+	}
+	
+	// returns size of object in memory
+	public static int size(ClassDecl decl) {
+		return size(decl.sym);
+	}
+	
+	// returns size of object in memory
+	public static int size(ClassSymbol decl) {
+		return decl.o_table.size();
+	}
+}
---
+++ src/HW4/src/cd/backend/codegen/Context.java
@@ -0,0 +1,121 @@
+package cd.backend.codegen;
+import java.util.HashMap;
+import java.util.Map;
+
+import cd.Config;
+import cd.backend.codegen.RegisterManager.Register;
+import cd.ir.Ast.*;
+
+/**
+ * Context of each method in visitor. Contains handful information.
+ * 
+ * @author limo
+ *
+ */
+public class Context {
+	private ClassDecl _class;
+	private MethodDecl _method;
+	
+	private Map<String, Integer> offsets;
+	
+	/**
+	 * Indicates whether you should return value or address.
+	 */
+	public boolean returnValue;
+	
+	/**
+	 * current offset from the stack pointer.
+	 */
+	public int SP_offset;
+	
+	/**
+	 * Creates new instance of Context class.
+	 * @param classDecl
+	 */
+	public Context(ClassDecl classDecl) {
+		this._class = classDecl;
+	}
+	
+	/**
+	 * Sets current method to the context
+	 * @param methodDecl Method to be set.
+	 * @param emit Assembly emitter.
+	 */
+	public void setMethod(MethodDecl methodDecl, AssemblyEmitter emit) {
+		this._method = methodDecl;
+		this.offsets = new HashMap<String, Integer>();
+		
+		int offset = 2 * Config.SIZEOF_PTR;
+		offsets.put("this", 2 * Config.SIZEOF_PTR);
+		// arguments have positive offset, all are of size 4
+		for(String arg : methodDecl.argumentNames) {
+			offset += Config.SIZEOF_PTR;
+			offsets.put(arg, offset);
+		}
+		// locals have negative offsets
+		offset = 0;
+		for(String local : methodDecl.sym.locals.keySet()) {
+			offset -= Config.SIZEOF_PTR;
+			offsets.put(local, offset);
+			emit.emitStore(AssemblyEmitter.constant(0), offset, Register.EBP);
+		}
+		
+	}
+	
+	/**
+	 * Gets offset of variable
+	 */
+	public int getOffset(String var) {
+		return this.offsets.get(var);
+	}
+	
+	/**
+	 * Gets current class
+	 */
+	public ClassDecl getCLass() {
+		return this._class;
+	}
+	
+	/**
+	 * Gets current method
+	 */
+	public MethodDecl getMethod() {
+		return this._method;
+	}
+	
+	/**
+	 * Gets current method label
+	 */
+	public String getMethodLabel() {
+		assert(_class != null);
+		assert(_method != null);
+		
+		return BackendUtils.getMethodLabel(this._class.name, this._method.name);
+	}
+	
+	/**
+	 * Gets method label
+	 * @param m_name name of the method to return label for
+	 */
+	public String getMethodLabel(String m_name) {
+		assert(_class != null);
+		
+		return BackendUtils.getMethodLabel(this._class.name, m_name);
+	}
+	
+	/**
+	 * Gets current vtable
+	 */
+	public VTable getVTable() {
+		assert(_class != null);
+		return _class.sym.v_table;
+	}
+	
+	/**
+	 * Gets current otable.
+	 */
+	public OTable getOTable() {
+		assert(_class != null);
+		return _class.sym.o_table;
+	}
+}
