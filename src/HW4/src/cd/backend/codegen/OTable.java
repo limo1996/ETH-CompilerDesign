a1ed7ba7de360060e6b10f4b6cbb3b4a3ed78a54
Jakub Lichman <lichman.jakub@gmail.com>
2018-05-01 17:55:57 +0200

    VTable and OTable implemented.

---
+++ src/HW4/src/cd/backend/codegen/OTable.java
@@ -0,0 +1,48 @@
+package cd.backend.codegen;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cd.Config;
+import cd.ir.Symbol.ClassSymbol;
+
+/**
+ * Class that manages offsets for a class. Copies fields of super class.
+ * @author limo
+ *
+ */
+public class OTable {
+	private List<String> offsets;
+	
+	public OTable(ClassSymbol decl) {
+		this.offsets = new ArrayList<String>();
+		if(decl.equals(ClassSymbol.objectType))
+			offsets.add(BackendUtils.getVTableLabel(decl.name));
+		else {
+			offsets.addAll(decl.superClass.o_table.offsets);
+			offsets.set(0, BackendUtils.getVTableLabel(decl.name));
+			for(String key : decl.fields.keySet()) {
+				offsets.add(decl.name + "." + key);
+			}
+		}
+	}
+	
+	/**
+	 * Returns offset of the field in memory.
+	 * @param field of the object
+	 * @return offsets of the field.
+	 */
+	public int offsetOf(String field) {
+		int index = offsets.indexOf(field);
+		assert(index >= 0);
+		return index * Config.SIZEOF_PTR;
+	}
+	
+	/**
+	 * Gets object size.
+	 * @return size of the object
+	 */
+	public int size() {
+		return offsets.size();
+	}
+}
---
+++ src/HW4/src/cd/backend/codegen/VTable.java
@@ -0,0 +1,85 @@
+package cd.backend.codegen;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import cd.Config;
+import cd.ir.Symbol.ClassSymbol;
+
+/**
+ * Table in data section that takes care of calling the right method. 
+ * Can be emitted to asm but performs the same functions in java as well.
+ * 
+ * @author limo
+ *
+ */
+public class VTable {
+	// data class for storing method offset pair
+	private class MethodOffset {
+		public String method;
+		public int offset;
+		public MethodOffset(String method, int offset) {
+			this.method = method;
+			this.offset = offset; 
+		}
+	}
+	
+	private String name;
+	
+	// we need to keep key as method name so we override methods from super classes
+	private Map<String, MethodOffset> methods = new HashMap<String, MethodOffset>();
+	
+	// creates new instance of VTable
+	public VTable(ClassSymbol sym) {
+		this.name = sym.name;
+		if(sym.superClass != null) {
+			methods.put("parent", new MethodOffset(sym.superClass.v_table.name(), 0));
+			methods.putAll(sym.superClass.v_table.methods);
+		} else {
+			methods.put("parent", new MethodOffset("0", 0));
+		}
+		for(String key : sym.methods.keySet()) {
+			if(methods.containsKey(key)) {
+				MethodOffset mo = methods.get(key);
+				mo.method = label(key);
+			} else {
+				methods.put(key, new MethodOffset(label(key), methods.size()));
+			}
+		}
+	}
+	
+	// returns asm label of vtable's method
+	public String label(String method) {
+		return BackendUtils.getMethodLabel(name, method);
+	}
+	
+	// emits itself into emitter
+	public void emit(AssemblyEmitter emit) {
+		emit.emitComment("VTable for " + this.name);
+		emit.emitLabel(BackendUtils.getVTableLabel(name));
+		String[] finalLabels = new String[methods.size()];
+		for(MethodOffset mo : methods.values()) {
+			finalLabels[mo.offset] = mo.method;
+		}
+		for(String label : finalLabels) {
+			emit.emitConstantData(label);
+		}
+	}
+		
+	// name
+	public String name() {
+		return BackendUtils.getVTableLabel(name);
+	}
+	
+	// vtable's asm name
+	public String vname() {
+		return BackendUtils.getVTableLabel(name);
+	}
+	
+	// returns methos offset
+	public int methodOffset(String name) {
+		return methods.get(name).offset * Config.SIZEOF_PTR;
+	}
+}
